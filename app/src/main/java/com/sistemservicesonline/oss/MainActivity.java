179d1b8b3acd9ee3fbc043dd9dddf9057d805a86
OSS-Admin <42009016+OSS-Admin@users.noreply.github.com>
2018-08-02 07:12:52 -0500

    Fecha           : 2018-08-01 Desarrollador   : Manuel Osorio Ochoa

--- app/src/main/java/com/sistemservicesonline/oss/MainActivity.java
+++ app/src/main/java/com/sistemservicesonline/oss/MainActivity.java
@@ -1,13 +1,97 @@
 package com.sistemservicesonline.oss;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Instancia del drawer
+     */
+    private DrawerLayout drawerLayout;
+
+    /**
+     * Titulo inicial del drawer
+     */
+    private String drawerTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolbar(); // Setear Toolbar como action bar
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        int id = menuItem.getItemId();
+
+                        //Para evitar que el elemento actual se seleccione una y otra vez
+                        if (menuItem.isChecked()){
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            return false;
+                        }
+
+                        if (id == R.id.nav_home) {
+                            startActivity(new Intent(getApplicationContext(), InicioActivity.class));
+                        }
+
+                        drawerLayout.closeDrawer(GravityCompat.START);
+
+                        return true;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            getMenuInflater().inflate(R.menu.nav_menu, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
