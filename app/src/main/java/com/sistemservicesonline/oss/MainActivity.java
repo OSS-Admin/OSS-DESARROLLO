a76c318dd5e0cad58b08ede4c63b41c108131151
OSS-Admin <42009016+OSS-Admin@users.noreply.github.com>
2018-08-02 12:57:01 -0500

    Fecha           : 2018-08-02 Desarrollador   : Manuel Osorio Ochoa

--- app/src/main/java/com/sistemservicesonline/oss/MainActivity.java
+++ app/src/main/java/com/sistemservicesonline/oss/MainActivity.java
@@ -1,13 +1,13 @@
 package com.sistemservicesonline.oss;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         if (id == R.id.nav_home) {
-                            startActivity(new Intent(getApplicationContext(), InicioActivity.class));
+                            //startActivity(new Intent(getApplicationContext(), InicioActivity.class));
                         }
 
                         drawerLayout.closeDrawer(GravityCompat.START);
@@ -93,5 +93,24 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder mensaje=new AlertDialog.Builder(this);
+        mensaje.setTitle("¿Desea Salir de la Aplicacion?");
+        mensaje.setCancelable(false);
+        mensaje.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        mensaje.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        mensaje.show();
     }
 }
