45d67bf2e1d481cbce6a74e1ffbd7e003e76ac11
OSS-Admin <42009016+OSS-Admin@users.noreply.github.com>
2018-08-22 09:14:16 -0500

    Fecha           : 2018-08-22 Desarrollador   : Manuel Osorio Ochoa

---
+++ app/src/main/java/com/sistemservicesonline/oss/activities/UsuariosActivity.java
@@ -0,0 +1,15 @@
+package com.sistemservicesonline.oss.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sistemservicesonline.oss.R;
+
+public class UsuariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuarios);
+    }
+}
